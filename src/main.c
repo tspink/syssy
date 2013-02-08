f95c1a628a45589a1f7da6e688b169bf9247bddb
Tom Spink <s0457958@ardgay.inf.ed.ac.uk>
2013-02-08 15:43:44 +0000

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/* 
+ * main.c
+ * 
+ * Copyright (C) University of Edinburgh 2013
+ * Tom Spink <t.spink@sms.ed.ac.uk>
+ */
+#include <stdio.h>
+
+/**
+ * Application entry-point.
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
